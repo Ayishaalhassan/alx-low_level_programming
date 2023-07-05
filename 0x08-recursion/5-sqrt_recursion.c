ef5302d3e2e6a8f91a78daba0245195d84933b89
Ayishaalhassan <alhassanayisha543@gmail.com>
2023-07-05 16:09:04 -0700

    ./5-sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -13,7 +13,7 @@ if (n < 0)
 return (-1);
 return (actual_sqrt_recursion(n, 0));
 }
-int actual_sqrt_recursion(int n, int i)
+int actual_sqrt_recursion(int n, int i);
 {
 if (i * i > n)
 return (-1);
